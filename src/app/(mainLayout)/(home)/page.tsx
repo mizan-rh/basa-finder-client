f61bcb6b18065465bb282747c5d7eb904e96ee61
Ahsan Tonmoy <ahosant82@gmail.com>
2025-04-17 02:37:05 +0600

    add home page content boiler

---
+++ src/app/(mainLayout)/(home)/page.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      {/* add or create all home page info & content static  */}
+      {/* Bannar dynamic */}
+      <div className=""></div>
+      {/* populer items list dynamic */}
+      <div className=""></div>
+      {/* listing dynamic */}
+      <div className=""></div>
+      {/* add why couse us static  */}
+      <div className=""></div>
+      {/* home page sort slider static upcoming */}
+      <div className=""></div>
+      {/* clint review dynamic*/}
+      <div className=""></div>
+      {/* our partners static upcoming */}
+    </div>
+  );
+};
+
+export default page;
