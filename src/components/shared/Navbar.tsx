f66a6c36bddb0f80d2649cb454f6b8a1d7c949c9
Ahsan Tonmoy <ahosant82@gmail.com>
2025-04-17 03:22:35 +0600

    navigetion boiler add

--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -1,14 +1,82 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
+import brand from "@/assets/images/brand/basaFinder-md.png";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const Navbar = () => {
+  const pathname = usePathname();
+  const [scrolled, setScrolled] = useState(false);
+
+  // Add scroll event listener to apply shadow on scroll
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 10) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  // add menu list
+  const navLinks = [
+    { name: "Home", href: "/" },
+    { name: "About-Us", href: "/about" },
+    { name: "All-Rentals", href: "/listings" },
+    // add more menu if work onther routes
+
+    // ...(user ? [{ name: "Dashboard", href: `/${user.role}s/dashboard` }] : []),
+  ];
+
   return (
     <div>
       {/* navbar content */}
-      <div className=" bg-black h-10">
-        {/* brand */}
-        {/* menu */}
-        {/* user */}
-      </div>
+      <header
+        className={scrolled ? " bg-white " : "bg-black opacity-70 text-white"}
+      >
+        <div className="p-4 flex justify-between items-center">
+          {/* brand - website log*/}
+          <div className="">
+            <Image className="w-56" src={brand} alt="BasaFinder Logo" />
+          </div>
+          {/* menu */}
+          <div className="">
+            {/* Desktop Navigation */}
+            <nav className="hidden md:flex items-center space-x-3 lg:space-x-6">
+              {navLinks.map((link) => (
+                <Link
+                  key={link.name}
+                  href={link.href}
+                  className={`text-sm lg:text-base text-white hover:text-blue-600 transition ${
+                    pathname === link.href ? "font-medium text-blue-600" : ""
+                  }`}
+                >
+                  {link.name}
+                </Link>
+              ))}
+            </nav>
+
+            {/*  */}
+            <div className=""></div>
+          </div>
+
+          {/* user */}
+          <div className="">
+            {/* Cart with responsive spacing */}
+            <div className=""></div>
+
+            {/* User Authentication - Desktop dynamic is user exit user*/}
+            <Link href="/login">
+              <button className="rounded border-1 px-4">Login</button>
+            </Link>
+          </div>
+        </div>
+      </header>
     </div>
   );
 };
