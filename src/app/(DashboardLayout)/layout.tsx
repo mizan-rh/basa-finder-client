14482705112a973a2de6f1cea8431d48bab15b45
Ahsan Tonmoy <ahosant82@gmail.com>
2025-04-20 18:12:23 +0600

    add toggle on dashboard layout

--- src/app/(DashboardLayout)/layout.tsx
+++ src/app/(DashboardLayout)/layout.tsx
@@ -1,26 +1,50 @@
+"use client";
 import { AppSidebar } from "@/components/modules/dashboard/sidebar/app-sidebar";
 import {
   SidebarInset,
   SidebarProvider,
   SidebarTrigger,
 } from "@/components/ui/sidebar";
+import { useState } from "react";
 
 export default function DashboardLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const [toggle, setToggle] = useState(true);
+  // const hendleToggle = () =>{
+  //   setToggle()
+  // }
   return (
     <SidebarProvider>
       <AppSidebar />
-      <SidebarInset>
-        <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
-          <div className="flex items-center gap-2 px-4 ">
-            <SidebarTrigger className="-ml-1 " />
-          </div>
-        </header>
-        <div className="p-4 pt-0 min-h-screen ">{children}</div>
-      </SidebarInset>
+      {/* toggle */}
+      {toggle ? (
+        <SidebarInset className=" md:ml-60">
+          <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
+            <div className="flex items-center gap-2 px-4 ">
+              <SidebarTrigger
+                onClick={() => setToggle(!toggle)}
+                className="-ml-1"
+              />
+            </div>
+          </header>
+          <div className="p-4 pt-0 min-h-screen ">{children}</div>
+        </SidebarInset>
+      ) : (
+        <SidebarInset className=" md:ml-16">
+          <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
+            <div className="flex items-center gap-2 px-4 ">
+              <SidebarTrigger
+                onClick={() => setToggle(!toggle)}
+                className="-ml-1"
+              />
+            </div>
+          </header>
+          <div className="p-4 pt-0 min-h-screen ">{children}</div>
+        </SidebarInset>
+      )}
     </SidebarProvider>
   );
 }
