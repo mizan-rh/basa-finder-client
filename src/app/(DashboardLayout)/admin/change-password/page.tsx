98f1cb5915e1790de7fb94f4d402a54a24383bca
Mizanur Rahman <mizanrxb@gmail.com>
2025-04-24 04:45:49 +0600

    path revalidate

--- src/app/(DashboardLayout)/admin/change-password/page.tsx
+++ src/app/(DashboardLayout)/admin/change-password/page.tsx
@@ -1,11 +1,13 @@
-import ChangePassword from '@/app/(MainLayout)/change-password/page'
+// import ChangePassword from '@/app/(MainLayout)/change-password/page'
+
+import ChangePassword from "../../../(MainLayout)/change-password/page";
 
 const changePasswordPage = () => {
   return (
     <div>
-        <ChangePassword />
+      <ChangePassword />
     </div>
-  )
-}
+  );
+};
 
-export default changePasswordPage
+export default changePasswordPage;
